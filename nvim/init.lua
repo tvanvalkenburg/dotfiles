39f5ec875e5689166905d7cce0f5e01db9b9967d
Taylor van Valkenburg <tvanvalkenburg@pm.me>
2026-05-21 17:49:21 +0200

    updated keybindings for search in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -346,7 +346,14 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep {
+          additional_args = function()
+            return { '--glob', '!tests/**', '--glob', '!test/**', '--glob', '!**/test_*.py', '--glob', '!**/*_test.py' }
+          end,
+        }
+      end, { desc = '[S]earch by [G]rep (no tests)' })
+      vim.keymap.set('n', '<leader>sG', builtin.live_grep, { desc = '[S]earch by [G]rep (all)' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
