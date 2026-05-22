42794bc8d1396ddccece9138f4004ae968acfc4e
Taylor van Valkenburg <tvanvalkenburg@pm.me>
2026-05-22 14:22:13 +0200

    updated search patterns

--- nvim/init.lua
+++ nvim/init.lua
@@ -343,9 +343,21 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files {
+          file_ignore_patterns = { 'tests/', 'test/', 'test_.*%.py', '.*_test%.py' },
+        }
+      end, { desc = '[S]earch [F]iles (no tests)' })
+      vim.keymap.set('n', '<leader>sF', builtin.find_files, { desc = '[S]earch [F]iles (all)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>sw', function()
+        builtin.grep_string {
+          additional_args = function()
+            return { '--glob', '!tests/**', '--glob', '!test/**', '--glob', '!**/test_*.py', '--glob', '!**/*_test.py' }
+          end,
+        }
+      end, { desc = '[S]earch current [W]ord (no tests)' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>sW', builtin.grep_string, { desc = '[S]earch current [W]ord (all)' })
       vim.keymap.set('n', '<leader>sg', function()
         builtin.live_grep {
           additional_args = function()
