8557e86a4d4ff913783385a341e7583e276518a9
Taylor van Valkenburg <tvanvalkenburg@pm.me>
2026-06-14 15:52:31 +0200

    updated wezterm default window size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,8 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
 config.font_size = 13.0
+config.initial_cols = 160
+config.initial_rows = 48
 
 config.line_height = 1.0
 
